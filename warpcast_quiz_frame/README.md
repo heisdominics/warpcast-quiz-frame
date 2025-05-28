3c85b4e7daa028dff6b1ed6c4cb82f81d19942b9
heisdominics <ogunmoladominics@gmail.com>
2025-05-28 23:21:01 +0100

    Add files via upload

---
+++ warpcast_quiz_frame/README.md
@@ -0,0 +1,3 @@
+# Warpcast Quiz Frame
+
+A basic quiz app designed to be used as a Warpcast Frame.
---
+++ warpcast_quiz_frame/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Welcome to the Warpcast Quiz</h1>
+      <p>This is a simple trivia quiz. More features coming soon!</p>
+    </main>
+  );
+}
